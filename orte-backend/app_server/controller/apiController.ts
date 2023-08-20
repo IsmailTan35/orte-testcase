006e52420ed7f1f50efa7439e794a2027a8b7db8
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-08-20 12:37:37 +0300

    fixed img api

--- orte-backend/app_server/controller/apiController.ts
+++ orte-backend/app_server/controller/apiController.ts
@@ -6,8 +6,25 @@ const apiController = (app: any) => {
     res.send("Orte TestCase - Hello World!");
   });
 
-  const publicImagePath = path.join(__dirname + "/../../pictures");
-  app.use("/api/img", express.static(publicImagePath));
+  app.get("/api/img/:filename", (req: any, res: any, next: any) => {
+    let rawPath = `/pictures/`;
+    var options = {
+      root: path.join(path.resolve(), `${rawPath}`),
+      dotfiles: "deny",
+      headers: {
+        "x-timestamp": Date.now(),
+        "x-sent": true,
+      },
+    };
+    var fileName = req.params.filename;
+    res.sendFile(fileName, options, function (err: any) {
+      if (err) {
+        next();
+      } else {
+        console.info("Sent:", fileName);
+      }
+    });
+  });
 };
 
 export default apiController;
