75ea58476471a944ee3a3a3f6f73e9e309698468
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-08-20 16:02:09 +0300

    last commit

--- orte-backend/app_server/models/dbConfig.ts
+++ orte-backend/app_server/models/dbConfig.ts
@@ -1,5 +1,5 @@
 export default {
-  host: process.env.MYSQL_HOST || "mysql_server",
+  host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "orte",
   database: process.env.MYSQL_DATABASE || "test_db",
